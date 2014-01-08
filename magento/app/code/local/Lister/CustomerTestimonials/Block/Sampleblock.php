2457089763c8ae6e18b21090371c1a041b1b5759
Sathishkumar <sathishkumar_v@LT-PC-301B.(none)>
2014-01-08 18:34:54 +0530

    Deleted Customer Testimonial

--- magento/app/code/local/Lister/CustomerTestimonials/Block/Sampleblock.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Lister_CustomerTestimonials_Block_Sampleblock extends Mage_Core_Block_Template {
-    
-
-}
-
-?>
--- magento/app/code/local/Lister/CustomerTestimonials/Model/Resource/Setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Lister_CustomerTestimonials_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup{
-    
-
-}
-?>
