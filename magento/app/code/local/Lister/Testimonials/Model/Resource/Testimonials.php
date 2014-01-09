2f03b32a05523fcb793fe9ebb9c93ba134538d04
Sathishkumar <sathishkumar_v@LT-PC-301B.(none)>
2014-01-09 16:25:28 +0530

    removed

--- magento/app/code/local/Lister/Testimonials/Model/Resource/Testimonials.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Lister_Testimonials_Model_Resource_Testimonials extends Mage_Core_Model_Resource_Db_Abstract{
-    protected function _construct()
-    {
-        $this->_init('testimonials/testimonials', 'testimonials_id');
-    }
-}
